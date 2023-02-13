b971bde729c148ab53f82db70721c9990e3d0927
ttapia <tomas@platan.us>
2023-02-13 10:36:27 -0300

    feat(application.js): register base components

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,25 @@
 import { createApp } from 'vue';
 import App from './components/app.vue';
+import BaseButton from './components/base-button.vue';
+import BaseInput from './components/base-input.vue';
+import BaseLabel from './components/base-label.vue';
 import './css/application.css';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const globalComponents = {
+    BaseButton,
+    BaseInput,
+    BaseLabel,
+  };
+
   const app = createApp({
     components: { App },
   });
+
+  Object.entries(globalComponents).forEach(([name, component]) => {
+    app.component(name, component);
+  });
+
   app.mount('#vue-app');
 
   return app;
