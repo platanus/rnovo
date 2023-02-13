ae812d64f55c475b6e9d9064ddcabd9cb46bf700
ttapia <tomas@platan.us>
2023-02-13 12:59:10 -0300

    chore(vue-i18n): add basic configuration for ts

--- app/javascript/application.ts
+++ app/javascript/application.ts
@@ -1,10 +1,19 @@
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 import App from './components/app.vue';
 import BaseButton from './components/base-button.vue';
 import BaseInput from './components/base-input.vue';
 import BaseLabel from './components/base-label.vue';
 import TermSearch from './components/term-search.vue';
 import './css/application.css';
+import { Locales } from './locales/locales';
+
+const i18n = createI18n({
+  legacy: false,
+  locale: 'es',
+  fallbackLocale: 'es',
+  messages: Locales.messages,
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   const globalComponents = {
@@ -20,6 +29,8 @@ document.addEventListener('DOMContentLoaded', () => {
   Object.entries(globalComponents).forEach(([name, component]) => {
     app.component(name, component);
   });
+
+  app.use(i18n);
 
   app.mount('#vue-app');
 
