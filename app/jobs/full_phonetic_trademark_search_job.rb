f3328d5c3b829795bf504f907ad1b744b2135034
Carlos Paredes <cparedesr@uc.cl>
2023-02-15 17:00:46 -0300

    feat(FullPhoneticTrademarkSearchJob): add job to search brand and its derivations

---
+++ app/jobs/full_phonetic_trademark_search_job.rb
@@ -0,0 +1,26 @@
+class FullPhoneticTrademarkSearchJob < ApplicationJob
+  queue_as :default
+
+  def perform(brand_name, nice_classes)
+    @brand_name = brand_name
+    @nice_classes = nice_classes
+
+    results = []
+
+    brand_name_variations.map do |variation|
+      results << tmview_client.phonetic_search(variation)
+    end
+
+    results.flatten.uniq
+  end
+
+  private
+
+  def brand_name_variations
+    GenerateTrademarkVariationsJob.perform_now(@brand_name)
+  end
+
+  def tmview_client
+    @tmview_client ||= TmviewClient.new(nice_classes: @nice_classes)
+  end
+end
---
+++ spec/jobs/full_phonetic_trademark_search_job_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe FullPhoneticTrademarkSearchJob, type: :job do
+  let(:tmview_client) { instance_double(TmviewClient, phonetic_search: []) }
+
+  before do
+    allow(TmviewClient).to receive(:new).and_return(tmview_client)
+    allow(tmview_client).to receive(:phonetic_search).and_return([])
+    allow(GenerateTrademarkVariationsJob)
+      .to receive(:perform_now)
+      .and_return(['variation1', 'variation2'])
+  end
+
+  describe '#perform' do
+    let(:brand_name) { 'brand_name' }
+    let(:nice_classes) { [create(:nice_class)] }
+
+    it 'calls GenerateTrademarkVariationsJob' do
+      described_class.perform_now(brand_name, nice_classes)
+      expect(GenerateTrademarkVariationsJob).to have_received(:perform_now).with(brand_name)
+    end
+
+    it 'calls TmviewClient#phonetic_search' do
+      described_class.perform_now(brand_name, nice_classes)
+      expect(tmview_client).to have_received(:phonetic_search).twice
+    end
+
+    it 'returns an array' do
+      expect(described_class.perform_now(brand_name, nice_classes)).to be_a(Array)
+    end
+  end
+end
