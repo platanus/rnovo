0046f8c6e5373b0ee3048b8fd36014f35c73f5f2
Carlos Paredes <cparedesr@uc.cl>
2023-02-20 16:38:47 -0300

    feat(PhoneticTrademarkSearch): add job to abstractize search

---
+++ spec/jobs/phonetic_trademark_search_job_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe PhoneticTrademarkSearchJob, type: :job do
+  let(:tmview_client) { instance_double(TmviewClient, phonetic_search: []) }
+
+  before do
+    allow(TmviewClient).to receive(:new).and_return(tmview_client)
+  end
+
+  describe '#perform' do
+    let(:brand_name) { 'brand_name' }
+    let(:nice_classes) { [create(:nice_class)] }
+
+    it 'calls TmviewClient#phonetic_search' do
+      described_class.perform_now(brand_name, nice_classes)
+      expect(tmview_client).to have_received(:phonetic_search)
+    end
+
+    it 'returns an array' do
+      expect(described_class.perform_now(brand_name, nice_classes)).to be_a(Array)
+    end
+  end
+end
