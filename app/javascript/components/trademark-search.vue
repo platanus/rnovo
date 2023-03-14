16b57bfad00c44758f448693dbc8d58c06d9b953
Carlos Paredes <cparedesr@uc.cl>
2023-03-14 12:46:17 -0300

    feat(TrademarkSearch): style change to mobile responsiveness

--- app/javascript/components/trademark-search.vue
+++ app/javascript/components/trademark-search.vue
@@ -55,101 +55,110 @@ function submit() {
 
 </script>
 <template>
-  <div class="my-5 mx-auto w-1/2 rounded-lg border bg-slate-100 p-5">
-    <div class="flex flex-col gap-5">
-      <base-input
-        v-model="search.name"
-        required
-        :label="t('trademarkSearch.searchLabel')"
-        name="name"
-        class="w-full"
-      />
+  <div class="px-6 md:py-6">
+    <div class="my-5 mx-auto rounded-lg border bg-slate-100 p-2 md:w-1/2 md:p-5">
+      <div class="flex flex-col gap-5">
+        <base-input
+          v-model="search.name"
+          required
+          :label="t('trademarkSearch.searchLabel')"
+          name="name"
+          class="w-full"
+        />
 
-      <base-multiselect
-        v-model="search.niceClassIds"
-        :options="niceClassOptions"
-        :label-hint="t('trademarkSearch.niceClassHint')"
-        :placeholder="t('trademarkSearch.niceClassPlaceholder')"
-        searchable
-        required
-        name="niceClassIds"
-      />
+        <base-multiselect
+          v-model="search.niceClassIds"
+          :options="niceClassOptions"
+          :label-hint="t('trademarkSearch.niceClassHint')"
+          :placeholder="t('trademarkSearch.niceClassPlaceholder')"
+          searchable
+          required
+          name="niceClassIds"
+        />
 
-      <base-button
-        @click="submit"
-      >
-        {{ t('trademarkSearch.search') }}
-      </base-button>
-    </div>
-    <div
-      v-if="isError"
-    >
-      <p>{{ t('trademarkSearch.error') }}</p>
-    </div>
-    <div
-      v-else-if="isFetching"
-      class="text-sm"
-    >
-      <p>{{ t('trademarkSearch.loading') }}</p>
-    </div>
-    <div
-      v-else-if="trademarks?.length > 0"
-      class="mt-5"
-    >
-      <h2 class="mb-2 text-2xl font-bold">
-        {{ t('trademarkSearch.results') }}
-      </h2>
+        <base-button
+          @click="submit"
+        >
+          {{ t('trademarkSearch.search') }}
+        </base-button>
+      </div>
       <div
-        v-for="trademark in trademarks"
-        :key="trademark.applicationNumber"
-        class="my-2 flex flex-col gap-5"
+        v-if="isError"
       >
-        <div class="flex w-full flex-col justify-between rounded-lg border bg-slate-200 p-2">
-          <div class="flex justify-between">
-            <h3 class="text-lg">
-              {{ trademark.trademarkName }} - {{ trademark.trademarkOffice }}
-            </h3>
+        <p>{{ t('trademarkSearch.error') }}</p>
+      </div>
+      <div
+        v-else-if="isFetching"
+        class="text-sm"
+      >
+        <p>{{ t('trademarkSearch.loading') }}</p>
+      </div>
+      <div
+        v-else-if="trademarks?.length > 0"
+        class="mt-5"
+      >
+        <div class="flex flex-col justify-between md:flex-row">
+          <h2 class="mb-2 text-2xl font-bold">
+            {{ t('trademarkSearch.results') }}
+          </h2>
+          <base-button
+            @click="refetchComplete"
+          >
+            {{ t('trademarkSearch.completeSearch') }}
+          </base-button>
+        </div>
+        <div
+          v-for="trademark in trademarks"
+          :key="trademark.applicationNumber"
+          class="my-2 flex flex-col gap-5"
+        >
+          <div class="flex w-full flex-col justify-between rounded-lg border bg-slate-200 p-2">
+            <div class="flex justify-between">
+              <h3 class="text-lg">
+                {{ trademark.trademarkName }} - {{ trademark.trademarkOffice }}
+              </h3>
+              <div class="hidden flex-col gap-1 md:flex">
+                <div class="flex gap-1">
+                  <span class="text-xs font-bold">
+                    {{ t('trademarkSearch.applicationNumber') }}
+                  </span>
+                  <span class="text-xs">
+                    {{ trademark.applicationNumber }}
+                  </span>
+                </div>
+              </div>
+            </div>
             <div class="flex flex-col gap-1">
               <div class="flex gap-1">
                 <span class="text-xs font-bold">
-                  {{ t('trademarkSearch.applicationNumber') }}
+                  {{ t('trademarkSearch.niceClass') }}:
                 </span>
                 <span class="text-xs">
-                  {{ trademark.applicationNumber }}
+                  {{ trademark.niceClasses?.join(', ') }}
                 </span>
               </div>
             </div>
-          </div>
-          <div class="flex flex-col gap-1">
-            <div class="flex gap-1">
-              <span class="text-xs font-bold">
-                {{ t('trademarkSearch.niceClass') }}:
-              </span>
-              <span class="text-xs">
-                {{ trademark.niceClasses?.join(', ') }}
-              </span>
-            </div>
-          </div>
 
-          <div class="flex flex-col gap-1">
-            <div class="flex gap-1">
-              <span class="text-xs font-bold">
-                {{ t('trademarkSearch.trademarkStatus') }}:
-              </span>
-              <span class="text-xs">
-                {{ t(`trademarkSearch.trademarkStatuses.${trademark.trademarkStatus}`) }}
-              </span>
+            <div class="hidden flex-col gap-1 md:flex">
+              <div class="flex gap-1">
+                <span class="text-xs font-bold">
+                  {{ t('trademarkSearch.trademarkStatus') }}:
+                </span>
+                <span class="text-xs">
+                  {{ t(`trademarkSearch.trademarkStatuses.${trademark.trademarkStatus}`) }}
+                </span>
+              </div>
             </div>
-          </div>
 
-          <div class="flex flex-col gap-1">
-            <div class="flex gap-1">
-              <span class="text-xs font-bold">
-                {{ t('trademarkSearch.score') }}:
-              </span>
-              <span class="text-xs">
-                {{ trademark.score }}%
-              </span>
+            <div class="hidden flex-col gap-1 md:flex">
+              <div class="flex gap-1">
+                <span class="text-xs font-bold">
+                  {{ t('trademarkSearch.score') }}:
+                </span>
+                <span class="text-xs">
+                  {{ trademark.score }}%
+                </span>
+              </div>
             </div>
           </div>
         </div>
@@ -198,7 +207,7 @@ function submit() {
         </div>
       </div>
       <div
-        v-else-if="trademarks?.length === 0"
+        v-else-if="trademarks?.length === 0 && trademarksComplete?.length === 0"
       >
         <p>{{ t('trademarkSearch.noResults') }}</p>
       </div>
